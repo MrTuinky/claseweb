f9476089767ae5c6fd718f893556426244a2c6d3
MrTuinky <carlos95egibide@gmail.com>
2019-10-03 19:04:10 +0200

    Tabla Angular, Controller y JSON

---
+++ Ejercicio15/controller.js
@@ -0,0 +1,28 @@
+app.controller('datosController', function($scope) {
+    $scope.nombres = [
+      { 
+          "Nombre" : "Karlos",
+          "Ciudad" : "Gasteiz",
+          "Provincia" : "Araba" 
+        },
+
+      { 
+          "Nombre" : "Patxi",
+          "Ciudad" : "Getxo",
+          "Provincia" : "Bizkaia" 
+        },
+
+      { 
+          "Nombre" : "Iker",
+          "Ciudad" : "Donosti",
+          "Provincia" : "Gipuzkoa" 
+        },
+
+      { 
+        "Nombre" : "Pablo",
+        "Ciudad" : "Bilbo",
+        "Provincia" : "Bizkaia" 
+    }
+
+    ];
+});
