fe6576a25dfd03026c2651d672224da61e8e8de3
MrTuinky <carlos95egibide@gmail.com>
2019-10-11 08:28:47 +0200

    Tabla de Usuarios Angular (Buena)

---
+++ Ejercicio15funciona/target/angularJS-1.0-SNAPSHOT/miTabla.js
@@ -0,0 +1,14 @@
+miModulo.controller('mio', function($scope, $http) {
+   
+    $scope.ver = function (){
+        var url='servlet';
+        var use;
+        $http.get(url).success(function(respuesta){
+            console.log("res:", respuesta);
+            use=respuesta;
+             console.log("USE:", use);
+              $scope.use=use;
+            });
+        }
+ 
+});
---
+++ Ejercicio15funciona/target/angularJS-1.0-SNAPSHOT/moduloTabla.js
@@ -0,0 +1,1 @@
+var miModulo = angular.module('miTabla',[]);
